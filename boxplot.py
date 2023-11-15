29ef6b4682b80b88124f3f783af32eacfa37c426
Marián Lorinc <malor23@student.sdu.dk>
2023-11-15 13:47:37 +0100

    Refactor data loading
    Signed-off-by: Marián Lorinc <malor23@student.sdu.dk>

--- boxplot.py
+++ boxplot.py
@@ -1,21 +1,8 @@
-import pandas as pd
 import seaborn as sns
 import re
-from enums import Algorithm, Map
 from matplotlib import pyplot as plt
 from pathlib import Path
-
-dependent_variables = [
-        "units traveled",
-        "error",
-        "rotation accumulator",
-        "time_taken"
-        ]
-
-independent_variables = [
-    "algorithm",
-    "map"
-]
+from loader import dependent_variables, load_data
 
 def generate_boxplot(data: str, output: str, format="png"):
     pattern = r"^[a-zA-Z0-9-]+$"
@@ -28,17 +15,13 @@ def generate_boxplot(data: str, output: str, format="png"):
     if output_path:
         output_path.mkdir(parents=True, exist_ok=True)
 
-    df = pd.read_csv(data, names=dependent_variables+independent_variables)
-    
-    df["algorithm"] = df["algorithm"].apply(lambda x: Algorithm(x).name)
-    df["map"] = df["map"].apply(lambda x: Map(x).name)
-
     # Set figure size to A4 landscape (in inches)
     a4_width_inch = 11.69  # 29.7 cm
     a4_height_inch = 8.27  # 21.0 cm
 
     plt.figure(figsize=(a4_width_inch, a4_height_inch))
 
+    df = load_data(data)
     for independent_var in ["algorithm"]:
         for dependent_var in dependent_variables:
             ax = sns.boxplot(data=df, y=dependent_var, x=independent_var, hue="map")
---
+++ loader.py
@@ -0,0 +1,40 @@
+import os
+import re
+import pandas as pd
+from enums import Algorithm, Map
+from pathlib import Path
+
+dependent_variables = [
+        "units traveled",
+        "error",
+        "rotation accumulator",
+        "time_taken"
+        ]
+
+independent_variables = [
+    "algorithm",
+    "map"
+]
+
+def get_files(root: str = "."):
+    # Define the pattern for the file names
+    pattern = re.compile(r"data_(\d+)_(\d+)\.txt")
+
+    # Get the list of files in the current directory
+    current_directory = Path(root)
+
+    files = [os.path.basename(root)] if not current_directory.is_dir() else os.listdir(root)
+
+    matches = [pattern.match(file) for file in files if pattern.match(file)]
+    return [(match.group(0), int(match.group(1)), int(match.group(2))) for match in matches]
+
+def load_data(root: str = "."):
+    database = pd.DataFrame(columns=dependent_variables+independent_variables)
+    for file, x, y in get_files(root):
+        df = pd.read_csv(file, names=dependent_variables+independent_variables)
+        df["algorithm"] = df["algorithm"].apply(lambda x: Algorithm(x).name)
+        df["map"] = df["map"].apply(lambda x: Map(x).name)
+        df["x"] = x
+        df["y"] = y
+        database = pd.concat((df, database))
+    return database
