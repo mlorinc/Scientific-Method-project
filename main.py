7774bc0066e349ea14960633a134afbc2471f425
Idinyte <black.mad.eagle@gmail.com>
2023-11-15 14:12:05 +0100

    recording

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from enum import Enum
 import numpy as np
 from queue import PriorityQueue
+import time
 
 def Render():
   # Fill the screen with the grid
@@ -222,7 +223,7 @@ class Direction(Enum):
   RU = 7
 
 
-############## CREATE OBSTACLE MAPS HERE, we'll need like 3 I guess #######################
+############## CREATE OBSTACLE MAPS HERE #######################
 def define_obstacles():
   obstacle_tv = [(4,0),(3,0)]
   obstacles_sofa = [(3,3), (4,3), (5,3), (3, 5), (4, 5), (6,3), (6,4), (6,5), (6,6), (5,5), (3, 6), (4, 6), (5, 6), (3, 4), (4, 4), (5, 4), (2,3), (2,4), (2,5), (2,6), (1,3), (1,4), (1,5), (1,6)]
@@ -248,9 +249,11 @@ def define_obstacles():
                   (17,11), (16,11), (15,11),
                   (15,12), (15,13), (15,14),
                   (16,14)]
+  
+  obstacle_smiley = [(1, 1), (1, 3), (3, 1), (3, 2), (3, 3)]
 
   obstacle_crazy = [(2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (2, 10), (2, 11), (2, 12), (2, 13), (2, 14), (2, 15), (2, 16), (2, 17), (2, 18), (2, 19), (2, 20), (2, 21), (2, 22), (2, 24), (2, 25), (2, 26), (2, 27), (3, 2), (3, 20), (3, 22), (3, 27), (4, 2), (4, 4), (4, 10), (4, 12), (4, 14), (4, 20), (4, 22), (4, 23), (4, 24), (4, 25), (4, 27), (5, 2), (5, 7), (5, 8), (5, 10), (5, 12), (5, 14), (5, 16), (5, 17), (5, 18), (5, 20), (5, 22), (5, 27), (6, 2), (6, 4), (6, 5), (6, 6), (6, 7), (6, 10), (6, 12), (6, 14), (6, 20), (6, 22), (6, 24), (6, 25), (6, 26), (6, 27), (7, 2), (7, 4), (7, 7), (7, 9), (7, 10), (7, 12), (7, 14), (7, 16), (7, 17), (7, 18), (7, 20), (7, 27), (8, 2), (8, 4), (8, 5), (8, 7), (8, 12), (8, 14), (8, 20), (8, 22), (8, 23), (8, 24), (8, 25), (8, 27), (9, 2), (9, 4), (9, 7), (9, 8), (9, 9), (9, 10), (9, 23), (9, 27), (10, 2), (10, 4), (10, 9), (10, 10), (10, 11), (10, 12), (10, 13), (10, 14), (10, 15), (10, 16), (10, 17), (10, 18), (10, 19), (10, 20), (10, 21), (10, 22), (10, 23), (10, 24), (10, 25), (10, 27), (11, 2), (11, 4), (11, 6), (11, 7), (11, 9), (11, 15), (11, 17), (11, 21), (11, 25), (11, 27), (12, 2), (12, 4), (12, 7), (12, 12), (12, 17), (12, 19), (12, 21), (12, 23), (12, 25), (12, 27), (13, 2), (13, 4), (13, 5), (13, 7), (13, 8), (13, 9), (13, 10), (13, 11), (13, 12), (13, 13), (13, 14), (13, 16), (13, 17), (13, 19), (13, 21), (13, 27), (14, 2), (14, 4), (14, 7), (14, 14), (14, 17), (14, 19), (14, 21), (14, 22), (14, 23), (14, 24), (14, 25), (14, 27), (15, 2), (15, 4), (15, 6), (15, 7), (15, 9), (15, 10), (15, 11), (15, 12), (15, 13), (15, 14), (15, 15), (15, 19), (15, 25), (15, 27), (16, 2), (16, 4), (16, 15), (16, 16), (16, 17), (16, 23), (16, 25), (16, 27), (17, 2), (17, 4), (17, 5), (17, 6), (17, 7), (17, 8), (17, 10), (17, 11), (17, 12), (17, 13), (17, 15), (17, 17), (17, 18), (17, 19), (17, 21), (17, 25), (17, 27), (18, 2), (18, 4), (18, 8), (18, 10), (18, 15), (18, 21), (18, 22), (18, 23), (18, 24), (18, 25), (18, 27), (19, 2), (19, 4), (19, 6), (19, 8), (19, 10), (19, 12), (19, 13), (19, 14), (19, 15), (19, 17), (19, 18), (19, 21), (19, 27), (20, 2), (20, 4), (20, 6), (20, 8), (20, 9), (20, 10), (20, 14), (20, 18), (20, 21), (20, 23), (20, 24), (20, 25), (20, 26), (20, 27), (21, 2), (21, 4), (21, 6), (21, 12), (21, 14), (21, 16), (21, 17), (21, 18), (21, 21), (21, 27), (22, 2), (22, 4), (22, 6), (22, 7), (22, 8), (22, 9), (22, 10), (22, 12), (22, 14), (22, 18), (22, 21), (22, 22), (22, 23), (22, 24), (22, 25), (22, 27), (23, 2), (23, 6), (23, 10), (23, 11), (23, 12), (23, 13), (23, 14), (23, 15), (23, 16), (23, 18), (23, 25), (23, 27), (24, 2), (24, 4), (24, 5), (24, 6), (24, 7), (24, 8), (24, 12), (24, 13), (24, 18), (24, 20), (24, 21), (24, 22), (24, 25), (24, 27), (25, 2), (25, 4), (25, 8), (25, 10), (25, 13), (25, 15), (25, 16), (25, 17), (25, 18), (25, 20), (25, 22), (25, 23), (25, 24), (25, 25), (25, 27), (26, 2), (26, 6), (26, 10), (26, 11), (26, 13), (26, 15), (26, 20), (26, 27), (27, 2), (27, 3), (27, 4), (27, 5), (27, 6), (27, 7), (27, 8), (27, 9), (27, 10), (27, 11), (27, 12), (27, 13), (27, 15), (27, 16), (27, 17), (27, 18), (27, 19), (27, 20), (27, 21), (27, 22), (27, 23), (27, 24), (27, 25), (27, 26), (27, 27)]
-  return obstacle_room, obstacle_maze, obstacle_crazy
+  return obstacle_room, obstacle_maze, obstacle_crazy, obstacle_smiley
 ###########################################################################################
 
 
@@ -413,6 +416,7 @@ class Map(Enum):
    Room = 1
    Maze = 2
    Crazy = 3
+   Smiley = 4
 ########################################################
 
 
@@ -420,6 +424,7 @@ def Start(algorithm, map, save = False):
     global rotation_accumulator, current_direction, units_traveled, EV3_path, error, grid
     global BLACK, WHITE, RED, CYAN, GRID_WIDTH, GRID_HEIGHT, GRID_SIZE
     global screen, font, WIDTH, SIDE_SCREEN_WIDTH, HEIGHT, font_size
+    global player_y, player_x
     key_pressed = False
     running = True
     path = []
@@ -430,16 +435,15 @@ def Start(algorithm, map, save = False):
     EV3_path = []
 
 
-
     ################### Adjust as needed ###################
     GRID_WIDTH = 30
     GRID_HEIGHT = 30
-    GRID_SIZE = 20 # size in pixels
+    GRID_SIZE = 30 # size in pixels
     ########################################################
 
     ########## Basic parameters - don't change #############
     pygame.init()
-    SIDE_SCREEN_WIDTH = 300
+    SIDE_SCREEN_WIDTH = 600
     WIDTH, HEIGHT = GRID_WIDTH * GRID_SIZE + SIDE_SCREEN_WIDTH, GRID_HEIGHT * GRID_SIZE
 
     CYAN = (0, 255, 255)
@@ -449,7 +453,7 @@ def Start(algorithm, map, save = False):
     font_size = 36
     font = pygame.font.Font(None, font_size)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Waka waka")
+    pygame.display.set_caption("Vacuum bot")
 
     grid = [[BLACK for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
     ########################################################
@@ -457,7 +461,7 @@ def Start(algorithm, map, save = False):
 
     ############ Load the selected obstacle map ############
     obstacle_map = []
-    obstacle_room, obstacle_maze, obstacle_crazy = define_obstacles()
+    obstacle_room, obstacle_maze, obstacle_crazy, obstacle_smiley = define_obstacles()
     if map == Map.Empty:
       obstacle_map = []
     elif map == Map.Room:
@@ -466,13 +470,16 @@ def Start(algorithm, map, save = False):
       obstacle_map = obstacle_maze
     elif map == Map.Crazy:
       obstacle_map = obstacle_crazy
+    elif map == Map.Smiley:
+      obstacle_map = obstacle_smiley
 
     for obstacle in obstacle_map:
         grid[obstacle[0]][obstacle[1]] = RED
     ########################################################
 
     grid[player_y][player_x] = WHITE # Start position is white
-
+    Render()
+    time.sleep(1)
 
     ############# Main while loop of the code ##############
     while running and any(BLACK in row for row in grid):
@@ -503,6 +510,8 @@ def Start(algorithm, map, save = False):
                 Update(next_step)
         elif algorithm == Algorithm.SemiRandom:
           semi_random()
+          
+        #time.sleep(0.25)
 
         Render()
     ########################################################
@@ -523,10 +532,9 @@ def Start(algorithm, map, save = False):
     return units_traveled, error, rotation_accumulator, time_taken
 
 ########################### Change selected algorithm and map #############################
-# algorithm = Algorithm.Manual
-# map = Map.Room
-
-# Start(algorithm, map)
+# algorithm = Algorithm.AStar
+# map = Map.Smiley
+# Start(algorithm, map, True)
 ###########################################################################################
 
 
@@ -545,8 +553,7 @@ def generate_data(algorithms, maps, test_trials, positions):
 
 player_y = 0
 player_x = 0
-#algorithms = [Algorithm.SemiRandom, Algorithm.AStartRandom, Algorithm.AStar, Algorithm.AStarSequential]
-algorithms = [Algorithm.AStar, Algorithm.AStarSequential]
+algorithms = [Algorithm.Random, Algorithm.SemiRandom, Algorithm.AStartRandom, Algorithm.AStarSequential, Algorithm.AStar]
 starting_positions = {
   Map.Empty: [(1, 0), (23, 27), (25, 3), (6, 19), (12, 14), (12, 27), (23, 7), (17, 16), (11, 3), (6, 11), (20, 17), (22, 5), (7, 0), (25, 23), (11, 1), (16, 6), (11, 25), (4, 27), (7, 16), (21, 7), (10, 20), (15, 15), (14, 5), (27, 3), (21, 6)],
   Map.Room: [(11, 1), (28, 21), (12, 21), (12, 29), (21, 23), (3, 25), (6, 11), (17, 1), (10, 5), (29, 2), (12, 26), (29, 22), (21, 0), (14, 12), (19, 7), (19, 10), (0, 14), (10, 16), (2, 10), (17, 21), (14, 22), (16, 28), (17, 2), (28, 24), (10, 8)],
@@ -554,6 +561,6 @@ starting_positions = {
   Map.Crazy: [(26, 12), (29, 16), (3, 5), (16, 26), (3, 3), (23, 29), (1, 29), (28, 5), (29, 0), (28, 26), (21, 19), (18, 1), (3, 8), (22, 17), (12, 15), (11, 13), (26, 0), (10, 1), (6, 8), (28, 12), (17, 3), (17, 23), (14, 9), (15, 16), (24, 0)]
 }
 maps = [Map.Empty, Map.Room, Map.Maze, Map.Crazy]
-test_trials = 25
+test_trials = 1
 generate_data(algorithms, maps, test_trials, starting_positions)
 ###########################################################################################
